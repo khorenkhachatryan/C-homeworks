cf91861225555a3c2a19774f355dfc779389f52f
khorenkhachatryan <124159763+khorenkhachatryan@users.noreply.github.com>
2023-02-10 15:15:34 +0400

    Add files via upload

---
+++ prob9.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+int main()
+{
+    std::string line;
+    std::fstream file;
+    file.open("text.txt");
+    if (file.is_open())
+    {
+        std::getline(file, line , '\n');
+        }
+    for(int i = 0; i < line.size(); i++)
+    {
+        if(line[i] >= 97 && line[i] <= 122)
+        {
+           line[i] = line[i] - 32;
+        }
+    }
+    std::cout << line << std::endl;    
+
+    system("pause");
+    return 0;
+
+}
